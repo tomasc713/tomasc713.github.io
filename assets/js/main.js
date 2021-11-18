16bfb341c3b07ca7392435f28b7fd2fe9db0c3ff
Tomas <tomasc713@outlook.com>
2021-11-17 22:54:07 -0500

    retoques y mejoras en la visualizacion de la pag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 const btnSwitch = document.querySelector('.navbar__switch');
 
-btnSwitch.addEventListener('click', ()=>{
-  btnSwitch.classList.toggle('navbar__switch--active');
-  document.body.classList.toggle('dark');
-})
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('navbar__switch--active');
+  
+    //local storage
+    if (document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//modo actual
+if (localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('navbar__switch--active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('navbar__switch--active');
+}
